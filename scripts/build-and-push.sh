4b1983c61e7f39e3932d445eeab80d2892114dc6
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2021-09-29 07:56:41 +0100

    2021

--- scripts/build-and-push.sh
+++ scripts/build-and-push.sh
@@ -6,6 +6,6 @@ cd ../website && \
 # origin  git@github.com:ULL-ESIT-DMSI-1920/ull-esit-dmsi-1920.github.io.git (fetch) \
 touch .nojekyll && \
 git add . && \
-git ci -am 'new build' && \
+git ci -am 'new build'; \
 git push --force origin master
 #git push --force sytws2021 master
