2ff8c562ffbbae5eeb5664eae96c4c4d9d3941e7
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2024-09-16 11:14:22 +0100

    2425

--- scripts/get-teams.sh
+++ scripts/get-teams.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ORG="ULL-ESIT-DMSI-2425"
-if [ $# -eq 1 ]; then
+if [ $# -eq 1 ]; then # See Rakefile target :teams
   ORG=$1
 fi
-TEAMS=$(gh org-teams -o "$ORG")
+TEAMS=$(gh org-teams -o "$ORG") # gh extension at https://github.com/gh-cli-for-education/gh-org-teams
 echo $TEAMS
